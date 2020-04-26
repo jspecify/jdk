8d12b237d876a7cf17e2fb347bb32e8c818967f9
Michael Ernst <mernst@cs.washington.edu>
2020-04-26 15:05:52 -0700

    Use annotation definitions from Checker Framework 3.3.0

--- src/java.base/share/classes/org/checkerframework/checker/nullness/qual/NonNull.java
+++ src/java.base/share/classes/org/checkerframework/checker/nullness/qual/NonNull.java
@@ -6,7 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.DefaultFor;
-import org.checkerframework.framework.qual.DefaultInUncheckedCodeFor;
 import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
 import org.checkerframework.framework.qual.LiteralKind;
 import org.checkerframework.framework.qual.QualifierForLiterals;
@@ -37,6 +36,9 @@ import org.checkerframework.framework.qual.UpperBoundFor;
  * @checker_framework.manual #nullness-checker Nullness Checker
  * @checker_framework.manual #bottom-type the bottom type
  */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @SubtypeOf(MonotonicNonNull.class)
 @QualifierForLiterals(LiteralKind.STRING)
 @DefaultQualifierInHierarchy
@@ -53,8 +55,4 @@ import org.checkerframework.framework.qual.UpperBoundFor;
             TypeKind.SHORT,
             TypeKind.BYTE
         })
-@DefaultInUncheckedCodeFor({TypeUseLocation.PARAMETER, TypeUseLocation.LOWER_BOUND})
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface NonNull {}
