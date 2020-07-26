fec5c7a6e99ce0fb40ce190f61bad706734956d4
Michael Ernst <mernst@cs.washington.edu>
2020-07-26 16:11:37 -0700

    Add `@InternedDistinct` annotation

--- src/jdk.compiler/share/classes/com/sun/tools/javac/code/Symbol.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/code/Symbol.java
@@ -25,6 +25,7 @@
 
 package com.sun.tools.javac.code;
 
+import org.checkerframework.checker.interning.qual.InternedDistinct;
 import org.checkerframework.checker.interning.qual.UsesObjectEquals;
 
 import java.lang.annotation.Annotation;
@@ -2106,7 +2107,7 @@ public abstract class Symbol extends AnnoConstruct implements Element {
         /** Dummy completer to be used when the symbol has been completed or
          * does not need completion.
          */
-        public final static Completer NULL_COMPLETER = new Completer() {
+        public final static @InternedDistinct Completer NULL_COMPLETER = new Completer() {
             public void complete(Symbol sym) { }
             public boolean isTerminal() { return true; }
         };
